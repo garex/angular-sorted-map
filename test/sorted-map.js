4ed9ade2ff10b435a27b98773320b8c1116c4b61
Ustimenko Alexander <ustimenko.alexander@gmail.com>
2016-03-27 05:30:48 +0600

    Add SortedMap tests

---
+++ test/sorted-map.js
@@ -0,0 +1,27 @@
+QUnit.module('SortedMap', {
+  beforeEach: function () {
+    this.filledMap = new garex.SortedMap()
+    this.filledMap
+      .put('a', null)
+      .put('z', 'z')
+      .put('p', 'p')
+
+    this.filledMap.remove('a')
+    this.filledMap.put('a', 'a')
+  }
+}, function () {
+  QUnit.module('put', function () {
+    QUnit.test('after adding keys sorted', function (assert) {
+      assert.deepEqual(this.filledMap.keys, ['a', 'p', 'z'])
+    })
+
+    QUnit.test('after adding values sorted', function (assert) {
+      assert.deepEqual(this.filledMap.values, ['a', 'p', 'z'])
+    })
+
+    QUnit.test('after adding entries sorted', function (assert) {
+      assert.equal(this.filledMap.entries[0].getKey(), 'a')
+      assert.equal(this.filledMap.entries[2].getKey(), 'z')
+    })
+  })
+})
